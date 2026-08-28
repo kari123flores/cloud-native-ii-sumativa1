967f0d3cc94ae5df081aca092c1705e78cb3caf3
kari123flores <karifloresich@gmail.com>
2026-08-28 18:06:30 -0400

    feat: crear microservicio BFF con Spring Boot

---
+++ bff-usuarios-roles/src/main/java/cl/duoc/bffusuariosroles/BffUsuariosRolesApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.bffusuariosroles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffUsuariosRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffUsuariosRolesApplication.class, args);
+	}
+
+}
---
+++ bff-usuarios-roles/src/test/java/cl/duoc/bffusuariosroles/BffUsuariosRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.bffusuariosroles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffUsuariosRolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
